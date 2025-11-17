9663943f4efc4aa8ad32ab3a2937cb18ad05ba68
Sharad Kumar Singh <Singhsharad529@gmail.com>
2025-11-17 21:09:05 +0530

    Fix SQLite database URL format

--- app/database.py
+++ app/database.py
@@ -6,7 +6,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "../.books.db")
 
-SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
+SQLALCHEMY_DATABASE_URL = f"sqlite:////{DB_PATH}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,connect_args={"check_same_thread":False}
